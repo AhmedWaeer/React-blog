2d4d4946f0af6934d20fcb955f77ab205e79de80
AhmedWaeer <78096972+AhmedWaeer@users.noreply.github.com>
2021-09-01 17:29:04 +0000

    Initial commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import Nav from '../component/Nav'
+import Hero from '../component/Hero'
+
+
+
+const HomePage = ()=> {
+
+  return(
+<div className="container mx-auto">
+   <Nav/>
+   <Hero/>
+  </div>
+  )
+}
+
+
+
+export default HomePage
